3cb4ce16f86fc6ddf0c229fb8e54c88307a52f3a
joao <joaosb004@gmail.com>
2023-02-13 14:17:21 -0300

    Solving code smells

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
